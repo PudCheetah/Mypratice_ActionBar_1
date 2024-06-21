472a9a32236b114cd42f6c3434cea6b944d4547d
PudCheetah <wer212131@gmail.com>
2024-06-21 11:44:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mypratice_actionbar_1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mypratice_actionbar_1
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mypratice_actionbar_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_item_1 -> {
+                Log.d("myTag", "MenuItem_1 has been catch")
+            }
+            R.id.menu_search -> {
+                Log.d("myTag", "MenuItem_2 has been catch")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
